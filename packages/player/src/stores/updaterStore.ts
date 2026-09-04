5f64efebb7150133bf2be8c63e85d08642a979a2
danidetenerife <12203716+danidetenerife@users.noreply.github.com>
2026-09-04 06:31:18 +0100

    auto: sync updates 2026-09-04 06:31:18

--- packages/player/src/stores/updaterStore.ts
+++ packages/player/src/stores/updaterStore.ts
@@ -10,7 +10,7 @@ import { errorMessage } from '../utils/errorMessage';
 import { reportError } from '../utils/logging';
 import { getSetting } from './settingsStore';
 
-const CURRENT_VERSION = '1.47.35';
+const CURRENT_VERSION = '1.47.36';
 const GITHUB_REPO = 'danidetenerife/nucelar';
 const GITHUB_LATEST_RELEASE_URL = `https://api.github.com/repos/${GITHUB_REPO}/releases/latest`;
 
